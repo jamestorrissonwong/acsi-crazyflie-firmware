83ae63459ad53bf3ce43d3af6747d20c60f27f5e
James Wong <jamestorrissonwong@gmail.com>
2022-12-12 16:00:01 -0500

    correct mass

--- src/modules/src/position_controller_pid.c
+++ src/modules/src/position_controller_pid.c
@@ -263,8 +263,9 @@ void velocityController(float* thrust, attitude_t *attitude, setpoint_t *setpoin
   // Thrust
   float m = 0.032;
   //float mass = 0.032;
+  mass = 0.037;
   // mass = 0.0347;
-  mass = 0.027;
+  // mass = 0.027;
   this.pidVZ.pid.kp = 25.0f * (mass/m);
   this.pidVZ.pid.ki = 15.0f * (mass/m);
 
